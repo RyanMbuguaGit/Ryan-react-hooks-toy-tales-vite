68ca8193e6f6bc347e7120fb290c254ede1eff15
RyanMbuguaGit <ryan.nganga@student.moringaschool.com>
2026-08-12 13:20:35 +0300

    Add PATCH fetch to like a toy

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 
-function ToyCard({ toy }) {
-  const { name, image, likes } = toy;
+function ToyCard({ toy, onUpdateToy }) {
+  const { id, name, image, likes } = toy;
+
+  function handleLikeClick() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: likes + 1 }),
+    })
+      .then((response) => response.json())
+      .then((updatedToy) => onUpdateToy(updatedToy));
+  }
 
   return (
     <div className="card" data-testid="toy-card">
       <h2>{name}</h2>
       <img src={image} alt={name} className="toy-avatar" />
       <p>{likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
+      <button className="like-btn" onClick={handleLikeClick}>
+        Like {"<3"}
+      </button>
       <button className="del-btn">Donate to GoodWill</button>
     </div>
   );
